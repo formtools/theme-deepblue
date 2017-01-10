aa32adc267fffe5fec5e017e5bc1025d7a763fb6
Ben Keen <ben.keen@gmail.com>
2017-01-09 19:43:44 -0800

    screenshot data added

--- about/theme.php
+++ about/theme.php
@@ -10,3 +10,6 @@ $theme_author_email = "formtools@encorewebstudios.com";
 $theme_link = "http://themes.formtools.org/deepblue/";
 $theme_description = "A fixed-width blue-coloured theme designed for 1024 pixel minimum screens.";
 $theme_version = "1.1.2";
+$theme_screenshots = array(
+		
+);
